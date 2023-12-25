bc55dd622b363534602a14f2b196bfe375213b49
ouissal23 <ouissal.aboussaid@gmail.com>
2023-12-25 23:16:09 +0100

    first commit

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+# The first 3 lettrers of the word
+print("First 3 letters: {}".format(word[0:3]))
+# The last 2 letters of the word
+print("Last 2 letters: {}".format(word[-2:]))
+# The middle word
+print("Middle word: {}".format(word[1:8]))
