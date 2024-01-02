e79a7cbf3d5d534e9d0aff611f6ba53c28d9bd78
ouissal23 <ouissal.aboussaid@gmail.com>
2024-01-02 22:39:34 +0100

    first commit

---
+++ python-functions/5-prime.py
@@ -0,0 +1,14 @@
+def is_prime(number):
+      if number<2:
+            return False
+      elif number>=2: 
+           for i in range (2,number):
+               if number%i==0:
+                  return False 
+      return True      
+               
+print(is_prime(17))
+print(is_prime(15))
+print(is_prime(-5))
+print(is_prime(0))
+       
