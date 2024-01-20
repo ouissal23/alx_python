3a8186488efdde1a4f0b7b6cf445da08fa8caf8f
ouissal23 <ouissal.aboussaid@gmail.com>
2024-01-20 02:00:44 +0100

    square commit

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+class  Square :
+    """Write a class that defines a square by private instance attribute: size."""
+
+    def __init__(self,size=0):
+        if not isinstance(size,int):
+            raise TypeError("size must be an integer")
+        if size <0:
+            raise ValueError("size must be>=0")
+
+        self.size = size
+    def area(self):
+        return(self.size*self.size)
