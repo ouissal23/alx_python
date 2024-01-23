fc049ac950c19c692930282897bde96b85e1efdf
ouissal23 <ouissal.aboussaid@gmail.com>
2024-01-23 02:23:13 +0100

    base commit

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,8 +1,9 @@
-class Base:
+class Base :
     __nb_objects = 0
     def __init__(self, id=None):
         if id is not None :
             self.id = id 
         else:
-            Base.__nb_objects +=1
-            self.id = self.__nb_objects    
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
+             
